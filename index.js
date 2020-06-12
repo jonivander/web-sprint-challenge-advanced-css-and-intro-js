416a1bd2228a76610596a0ae1168d15995c7ff4a
Jonathan Warner <jonivander@Jonathans-MacBook-Air-3.local>
2020-06-12 09:12:53 -0700

    completed tasks 1 though 4

--- index.js
+++ index.js
@@ -208,11 +208,17 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name)
+console.log(artists[2].bio)
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+function correctName(array, index, name){
+  array[index].name = name;
+  return array[index];
+}
+console.log(correctName(artists, 8, 'Vincent Van Gogh'))
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,20 +229,20 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return `The artist at index ${index} is ${array[index].name}.`
   }
-  
+  console.log(getArtistByIndex(artists, 0))
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who lived in the 20th century (1900-2000) */
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(){
+  for(i = 0; i <=array.length-1; i++){
+    if(array.years)
+  }
 }
-
+console.log()
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
@@ -248,10 +254,11 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
-  
+function removeArtist(array, index){
+  array.splice(index, 1);
+  return array;
+}
+console.log(removeArtist(artists, 0))
   /**
 
 
